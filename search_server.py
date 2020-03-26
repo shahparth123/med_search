072934e5b117db7ec349a59649b63c25553d1da3
Parth Shah <parthpunita@gmail.com>
2020-03-26 17:22:39 +0530

    Initial commit

---
+++ search_server.py
@@ -0,0 +1,118 @@
+# serve.py
+import re
+import os
+from os import listdir
+from os.path import isfile, join
+import sys
+import pprint
+from flask import Flask, redirect, url_for, request,render_template,jsonify
+path = 'demo'
+import glob
+import json
+global_data=list()
+# creates a Flask application, named app
+app = Flask(__name__,static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():  
+    message = "Hello, World"
+    """ return jsonify({
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}) """
+    return render_template('index.html', message=message)
+
+@app.route('/refresh', methods=['GET'])
+def refresh():
+    global global_data
+    all_data=list()
+
+    path_to_files = os.path.join( 'static','data','*',"*","00*.json")
+    for filename in sorted(glob.glob(path_to_files)):
+        with open(filename) as f:
+            print(filename)
+            data = json.load(f)
+            data['path']=filename
+            #print(data)
+            all_data.append(data)
+    global_data=all_data
+    return jsonify(all_data)
+
+@app.route('/search/<word>', methods=['GET'])
+def search(word):
+    global global_data
+    if request.method == 'GET':
+        print(word)
+        results=[]
+        print(len(global_data))
+        for paper in global_data:
+            title = paper['metadata']['title']
+            x = re.search(word, title)
+            print(x)
+            if(x):
+                result={
+                    "title":paper['metadata']['title'],
+                    "path":paper['path']
+                }
+                results.append(result)
+        return jsonify(results)
+
+@app.route('/searchmultipleor/<words>', methods=['GET'])
+def searchmultipleor(words):
+    global global_data
+    if request.method == 'GET':
+        word = words.split(" ")
+        print(word)
+        results={}
+        for item in word:
+            results[item]=[]
+            for paper in global_data:
+                title = paper['metadata']['title']
+                x = re.search(item, title)
+                print(x)
+                if(x):
+                    result={
+                        "title":paper['metadata']['title'],
+                        "path":paper['path']
+
+                    }
+                    results[item].append(result)
+    
+        return jsonify(results)
+
+@app.route('/searchmultipleand/<words>', methods=['GET'])
+def searchmultipleand(words):
+    global global_data
+    if request.method == 'GET':
+        word = words.split(" ")
+        print(word)
+        results=[]
+        for paper in global_data:
+            title = paper['metadata']['title']
+            flag=1
+            for item in word:
+                x = re.search(item, title)
+                if(x):
+                    pass
+                else:
+                    flag=0
+            if(flag==1):    
+                result={
+                    "title":paper['metadata']['title'],
+                    "path":paper['path']
+
+                }
+                results.append(result)
+        return jsonify(results)
+
+
+
+
+# run the application
+if __name__ == "__main__":
+    global_data=[]  
+    app.run(debug=True)
